afae6f37f848333da3c59c2366456c50ec533a64
Jawad Usman <jwdusmn@gmail.com>
2019-09-24 17:03:06 -0400

    Fixing 2 player controls
    - cleaning code
    - sepearating controls for p1 and p2

--- Shooter/Assets/Scripts/PlayerMovement.cs
+++ Shooter/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
-    [SerializeField] float jump;
-    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float moveSpeed = 25;
+    [SerializeField] float jump = 15;
+	
+    [SerializeField] Rigidbody2D rb1;
+	[SerializeField] Rigidbody2D rb2;
 	
 	[SerializeField] bool faceRight;
 
@@ -20,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        //rb = GetComponent<Rigidbody2D>();
 		faceRight = true;
     }
 
@@ -31,57 +33,56 @@ public class PlayerMovement : MonoBehaviour
 
         player2 = PlayerState2.player;
 
-        if (player == 1)
-        { 
-            //float moveHorizontal = Input.GetAxis("Horizontal");
-            //Vector2 move = new Vector2(moveHorizontal, 0);
-            if (Input.GetButton("Horizontal_p1"))
-            {
-                p1.transform.position += Input.GetAxis("Horizontal_p1") * transform.right * moveSpeed * Time.deltaTime;
-                //rb.AddForce(move * moveSpeed);
 
-                if (p1.transform.position.x > 0 && !faceRight)
-                {
-                    p1.transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-                    faceRight = true;
-                }
-                if (p1.transform.position.x < 0 && faceRight)
-                {
-                    p1.transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-                    faceRight = false;
-                }
-            }
-            if (Input.GetButton("Jump_p1"))
-            {
-                rb.AddForce(Vector2.up * jump);
-            }
-        }
-        else if (player2 == 2)
-        {
-            Debug.Log("her is" + player2);
-            //float moveHorizontal = Input.GetAxis("Horizontal");
-            //Vector2 move = new Vector2(moveHorizontal, 0);
-            if (Input.GetButton("Horizontal_p2"))
-            {
-                Debug.Log("right leftds;lkdfj");
-                p2.transform.position += Input.GetAxis("Horizontal_p2") * transform.right * moveSpeed * Time.deltaTime;
-                //rb.AddForce(move * moveSpeed);
+		//float moveHorizontal = Input.GetAxis("Horizontal");
+		//Vector2 move = new Vector2(moveHorizontal, 0);
+		if (Input.GetButton("Horizontal_p1"))
+		{
+			Debug.Log("aHHHHHHHHHHHHHH");
+			p1.transform.position += Input.GetAxis("Horizontal_p1") * transform.right * moveSpeed * Time.deltaTime;
+			//rb.AddForce(move * moveSpeed);
 
-                if (p2.transform.position.x > 0 && !faceRight)
-                {
-                    p2.transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-                    faceRight = true;
-                }
-                if (p2.transform.position.x < 0 && faceRight)
-                {
-                    p2.transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-                    faceRight = false;
-                }
-            }
-            if (Input.GetButton("Jump_p2"))
-            {
-                rb.AddForce(Vector2.up * jump);
-            }
-        }
+			if (p1.transform.position.x > 0 && !faceRight)
+			{
+				p1.transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+				faceRight = true;
+			}
+			if (p1.transform.position.x < 0 && faceRight)
+			{
+				p1.transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+				faceRight = false;
+			}
+		}
+		if (Input.GetButton("Jump_p1"))
+		{
+			rb1.AddForce(Vector2.up * jump);
+		}
+	
+   
+
+		//float moveHorizontal = Input.GetAxis("Horizontal");
+		//Vector2 move = new Vector2(moveHorizontal, 0);
+		if (Input.GetAxis("Horizontal_p2") == 1 || Input.GetAxis("Horizontal_p2") == -1)
+		{
+			Debug.Log("right leftds;lkdfj");
+			p2.transform.position += Input.GetAxis("Horizontal_p2") * transform.right * moveSpeed * Time.deltaTime;
+			//rb.AddForce(move * moveSpeed);
+
+			if (p2.transform.position.x > 0 && !faceRight)
+			{
+				p2.transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+				faceRight = true;
+			}
+			if (p2.transform.position.x < 0 && faceRight)
+			{
+				p2.transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+				faceRight = false;
+			}
+		}
+		if (Input.GetButton("Jump_p2"))
+		{
+			rb2.AddForce(Vector2.up * jump);
+		}
+        
     }
 }
